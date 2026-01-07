155ed028f8a2acaf21bf4851e3a1098bd92c3bfe
panlorex <mate-lorei@hotmail.com>
2026-01-07 00:09:48 -0500

    Archivos propios innecesarios eliminados

--- scripts/m1_setup_env.ps1
+++
@@ -1,5 +0,0 @@
-#este script crea el entorno virtual y lo activa, luego instala las dependencias necesarias
-python -m venv .venv
-.\.venv\Scripts\Activate.ps1
-python -m pip install --upgrade pip
-pip install -r requirements.txt
